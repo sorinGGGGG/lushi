02633afa8156cd01a71f5c6f9562690b80745d7e
卧云孤客 <w43020219950724@163.com>
2021-01-06 11:26:29 +0800

    服务中心更新

--- pages/serviceCentre/serviceCentre.js
+++ pages/serviceCentre/serviceCentre.js
@@ -5,26 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    scrollHeight: "100vh;",
+    isDetails: false,
+    isHighNew: true,
+    showDetails: '企业详情'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      scrollHeight: getApp().globalData.height + 'px'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow() {
     if (typeof this.getTabBar === 'function' &&
       this.getTabBar()) {
@@ -35,38 +30,19 @@ Page({
     this.onLoad()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  isDetails() {
+    this.setData({
+      isDetails: !this.data.isDetails
+    })
+    if (this.data.isDetails) {
+      this.setData({
+        showDetails: '关闭详情'
+      })
+    } else {
+      this.setData({
+        showDetails: '企业详情'
+      })
+    }
   }
+
 })
